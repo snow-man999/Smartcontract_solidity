6334c3e5510ca3718b23045f3add31602581c345
Snowstorm08 <snowmandot0520@gmail.com>
2026-01-28 20:15:36 +0900

    Refactor Caller contract and update Solidity version
    Updated the solidity version and improved the externalCall function with error handling.

--- src/test/utils/Caller.sol
+++ src/test/utils/Caller.sol
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 contract Caller {
-    /// @dev Can use this method to call any other contract's function
-    /// @param contractAddress_ Address of the contract to call
-    /// @param callData_ Call data
-    /// @return ok is `true` if the call was successful
-    /// @return data is the encoded result of the call
-    function externalCall(address contractAddress_, bytes calldata callData_)
-        external
-        returns (bool, bytes memory)
-    {
-        (bool ok, bytes memory data) = contractAddress_.call(callData_); // solhint-disable-line
-        return (ok, data);
+    error CallFailed(address target, bytes data);
+
+    /**
+     * @notice Calls an external contract with arbitrary calldata
+     * @param target Address of the contract to call
+     * @param data Calldata to send
+     * @return returnData Raw returned data from the call
+     */
+    function externalCall(
+        address target,
+        bytes calldata data
+    ) external payable returns (bytes memory returnData) {
+        require(target != address(0), "Invalid target");
+
+        (bool success, bytes memory result) = target.call{value: msg.value}(data);
+
+        if (!success) {
+            revert CallFailed(target, result);
+        }
+
+        return result;
     }
 }
